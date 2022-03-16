7d8f28509b6b90fc8b577690cb29bb8e48e59dc6
rq-yuseiuchida-at-321899771753 <yusei@Yuseis-MacBook-Air.local>
2022-03-16 20:45:28 +0900

    setup sorcery

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,16 @@
+class UserSessionsController < ApplicationController
+	def create
+    @user = login(params[:email], params[:password])
+
+    if @user
+      redirect_to root_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to root_path
+  end
+end
