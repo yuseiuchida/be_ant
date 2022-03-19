88f701ecbae8ed4f6ce64fe2f58cad5896338551
rq-yuseiuchida-at-321899771753 <yusei@Yuseis-MacBook-Air.local>
2022-03-19 18:07:17 +0900

    リアクション機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :goals
   has_many :records
+  has_many :reactions
 
   authenticates_with_sorcery!
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
