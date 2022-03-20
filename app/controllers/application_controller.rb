c2f111289b94b30c531404bc60f5b712cd3390c1
rq-yuseiuchida-at-321899771753 <yusei@Yuseis-MacBook-Air.local>
2022-03-20 22:23:20 +0900

    adminlte設定からログイン画面まで

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	before_action :require_login
+	private
+
+	def not_authenticated
+    flash[:warning] = t('defaults.message.require_login')
+    redirect_to login_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
   validates :email, uniqueness: true
+
+  enum role: { general: 0, admin: 1 }
 end
