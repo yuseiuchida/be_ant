4a1a99a6ac66f5c9c4e2f32f69c6eacbc2cdca2c
rq-yuseiuchida-at-321899771753 <yusei@Yuseis-MacBook-Air.local>
2022-03-22 12:57:25 +0900

    admin users_controller作成

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,7 +1,7 @@
 class Record < ApplicationRecord
 	belongs_to :user
 	belongs_to :goal
-	has_many :reactions
+	has_many :reactions, dependent: :destroy
 
 	enum bug_status: { アリ: 1, キリギリス: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ApplicationRecord
-  has_many :goals
-  has_many :records
-  has_many :reactions
+  has_many :goals, dependent: :destroy
+  has_many :records, dependent: :destroy
+  has_many :reactions, dependent: :destroy
 
   authenticates_with_sorcery!
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :email, uniqueness: true
+  validates :email, :name, uniqueness: true
+  validates :email, :name, presence: true
 
   enum role: { general: 0, admin: 1 }
 end
